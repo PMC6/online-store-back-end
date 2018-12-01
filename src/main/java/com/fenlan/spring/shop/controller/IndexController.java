1448885253d1a7757e05f564922ec9e0d572b445
fenlan <1475307818@qq.com>
2018-12-01 22:14:59 +0800

    自定义登录

--- src/main/java/com/fenlan/spring/shop/controller/IndexController.java
+++ src/main/java/com/fenlan/spring/shop/controller/IndexController.java
@@ -6,7 +6,11 @@ import com.fenlan.spring.shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,15 +23,50 @@ public class IndexController {
     UserService service;
     @Autowired
     private HttpServletRequest request;
+    @Autowired
+    AuthenticationManager manager;
 
     @GetMapping("")
     public ResponseEntity<ResponseFormat> index(Authentication auth) {
-
+        System.out.println("------------------------------------");
         return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
                 .error(null)
                 .message("login success")
                 .path(request.getServletPath())
                 .data(auth)
+                .build(), HttpStatus.OK);
+    }
+
+    @PostMapping("user/login")
+    public ResponseEntity<ResponseFormat> login(@RequestBody User param) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(param.getUsername(), param.getPassword());
+        try {
+            Authentication auth = manager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            System.out.println("fuck");
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("login success")
+                    .path(request.getServletPath())
+                    .data(auth)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("login fail")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("test")
+    public ResponseEntity<ResponseFormat> test(@RequestParam("username") String username) {
+        return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                .error(null)
+                .message("login success")
+                .path(request.getServletPath())
+                .data(username)
                 .build(), HttpStatus.OK);
     }
 
