e04e1acf543783a0811ebd70f6e93e58ad30a401
fanzhonghao <1412328318@qq.com>
2018-12-02 20:50:20 +0800

    seller controller test

--- src/main/java/com/fenlan/spring/shop/controller/seller/ShopControl.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/ShopControl.java
@@ -6,5 +6,32 @@
  */
 package com.fenlan.spring.shop.controller.seller;
 
+import com.fenlan.spring.shop.bean.Product;
+import com.fenlan.spring.shop.bean.Shop;
+import com.fenlan.spring.shop.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shop/info")
 public class ShopControl {
+    @Autowired
+    ShopService shopService;
+
+
+    @RequestMapping
+    public String allShop(Model model){
+        List<Shop> shopList = null;
+        try {
+            shopList = shopService.findAllShop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        model.addAttribute("shops", shopList);
+        return "shopInfo";
+    }
 }
--- src/main/java/com/fenlan/spring/shop/controller/seller/loginControl.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/loginControl.java
@@ -6,5 +6,26 @@
  */
 package com.fenlan.spring.shop.controller.seller;
 
-public class loginControl {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//@Controller
+//@RequestMapping("/seller")
+//public class loginControl {
+//    @RequestMapping("/login")
+//    public String login(Model model){
+//        return "login";
+//    }
+//}
+
+@Controller
+@RequestMapping("/")
+class login{
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
