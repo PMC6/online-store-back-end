3d5d8ce51fff30e27acb08e2c38cf39059dda66d
fanzhonghao <1412328318@qq.com>
2018-12-02 15:22:58 +0800

    add product and shop service

--- src/main/java/com/fenlan/spring/shop/service/ProductService.java
+++ src/main/java/com/fenlan/spring/shop/service/ProductService.java
@@ -1,16 +1,25 @@
 package com.fenlan.spring.shop.service;
 
+import com.fenlan.spring.shop.DAO.CategoryDAO;
 import com.fenlan.spring.shop.DAO.ProductDAO;
+import com.fenlan.spring.shop.DAO.ShopDAO;
+import com.fenlan.spring.shop.bean.Category;
 import com.fenlan.spring.shop.bean.Product;
+import com.fenlan.spring.shop.bean.Shop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Entity;
 import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     ProductDAO productDAO;
+    @Autowired
+    CategoryDAO categoryDAO;
+    @Autowired
+    ShopDAO shopDAO;
 
     public Product add(Product newProduct) throws Exception {
         try {
@@ -27,5 +36,70 @@ public class ProductService {
         if (list.size() == 0)
             throw new Exception("null");
         return list;
+    }
+
+    /**
+     * update at 18.12.1 by fan
+     * 由商品类别查找商品
+     * @param category
+     * @return
+     */
+    public List<Product> findByCategory(String category){
+        Category category1 = categoryDAO.findByName(category);
+        return productDAO.findByCategoryId(category1.getId());
+    }
+
+    /**
+     * update at 18.12.1 by fan
+     * 由商店名查找商品
+     * @param shopName
+     * @return
+     */
+    public List<Product> findByShopName(String shopName){
+        Shop shop = shopDAO.findByName(shopName);
+        return productDAO.findByShopId(shop.getId());
+    }
+
+    /**
+     * update at 18.12.1 by fan
+     * 由商品名查找商品
+     * @param productName
+     * @return
+     */
+    public List<Product> findByProductName(String productName){
+        return productDAO.findByName(productName);
+    }
+
+    /**
+     * update at 18.12.1 by fan
+     * 更新商品信息
+     * @param originalProduct
+     * @param targetProduct
+     */
+    public void updateProduct(Product originalProduct, Product targetProduct){
+        productDAO.delete(originalProduct);
+        productDAO.save(targetProduct);
+    }
+
+    /**
+     * update at 18.12.1 by fan
+     * 删除商品
+     * @param product 商品
+     */
+    public void deleteProduct(Product product){
+        try {
+            productDAO.delete(product);
+        }catch (Exception e){
+            System.out.println("delete product error at ProductService.java");
+        }
+    }
+
+    /**
+     * update at 18.12.1 by fan
+     * 删除每个商店的全部产品
+     * @param shop
+     */
+    public void deleteProductWithShop(Shop shop){
+        productDAO.deleteAllByShopId(shop.getId());
     }
 }
--- src/main/java/com/fenlan/spring/shop/service/ShopService.java
+++ src/main/java/com/fenlan/spring/shop/service/ShopService.java
@@ -1,11 +1,78 @@
-/***
+/**
  * @author： fanzhonghao
  * @date: 18-12-1 19 26
  * @version: 1.0
  * @description:
- *
+ *   提供商店信息查看与修改功能
  */
 package com.fenlan.spring.shop.service;
 
+import com.fenlan.spring.shop.DAO.ShopDAO;
+import com.fenlan.spring.shop.DAO.UserDAO;
+import com.fenlan.spring.shop.bean.Shop;
+import com.fenlan.spring.shop.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ShopService {
+    @Autowired
+    ShopDAO shopDAO;
+    @Autowired
+    UserDAO userDAO;
+    /**
+     * 保存商店信息
+     * @param shop
+     */
+    public void saveShop(Shop shop){
+        shopDAO.save(shop);
+    }
+
+    /**
+     * 更新商店信息
+     * @param originalShop 更改之前的商店信息
+     * @param targetShop 更改之后的商店信息
+     */
+    public void update(Shop originalShop, Shop targetShop){
+        shopDAO.delete(originalShop);
+        shopDAO.save(targetShop);
+    }
+
+    /**
+     * 由shop_id得到商店信息
+     * @param id shop id
+     * @return
+     */
+    public Shop findByShopId(long id){
+        return shopDAO.findById(id);
+    }
+
+    /**
+     * 由卖家名字得到其所有的商店的信息
+     * @param sellerName: seller name
+     * @return shop
+     */
+    public Shop findByOwnerName(String sellerName){
+        if(sellerName == null){
+            return null;
+        }
+        User user = userDAO.findByUsername(sellerName);
+        if (user == null){
+            return null;
+        }
+        return  shopDAO.findByUserId(user.getId());
+    }
+
+    /**
+     * 由卖家名删除商店信息
+     * @param sellerName
+     */
+    public void deleteShopByOwnerName(String sellerName){
+        User user = userDAO.findByUsername(sellerName);
+        if (user == null) return;
+        Shop shop = shopDAO.findByUserId(user.getId());
+        new ProductService().deleteProductWithShop(shop);
+        shopDAO.delete(shop);
+    }
 }
