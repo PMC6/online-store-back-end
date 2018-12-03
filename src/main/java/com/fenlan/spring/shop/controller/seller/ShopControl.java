22edf961cc3419585d8a5a5fbbab8f545db9fb93
fanzhonghao <1412328318@qq.com>
2018-12-03 13:28:37 +0800

    update seller controller

--- src/main/java/com/fenlan/spring/shop/controller/seller/ShopControl.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/ShopControl.java
@@ -10,20 +10,22 @@ import com.fenlan.spring.shop.bean.Product;
 import com.fenlan.spring.shop.bean.Shop;
 import com.fenlan.spring.shop.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/shop/info")
+//@RestController
+@Controller
+@RequestMapping("/")
 public class ShopControl {
     @Autowired
     ShopService shopService;
 
 
-    @RequestMapping
+    @RequestMapping("/login")
     public String allShop(Model model){
         List<Shop> shopList = null;
         try {
--- src/main/java/com/fenlan/spring/shop/controller/seller/loginControl.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/loginControl.java
@@ -7,25 +7,15 @@
 package com.fenlan.spring.shop.controller.seller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-//@Controller
-//@RequestMapping("/seller")
-//public class loginControl {
-//    @RequestMapping("/login")
-//    public String login(Model model){
-//        return "login";
-//    }
-//}
 
 @Controller
-@RequestMapping("/")
-class login{
+@RequestMapping("/seller")
+public class loginControl {
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
+
 }
