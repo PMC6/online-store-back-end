defa7a126fc4bd1b2312ff569f334ccdcfbfc077
fenlan <1475307818@qq.com>
2018-12-10 16:27:04 +0800

    添加修改密码和获取个人信息

---
+++ src/main/java/com/fenlan/spring/shop/controller/customer/CustomerController.java
@@ -0,0 +1,62 @@
+package com.fenlan.spring.shop.controller.customer;
+
+import com.fenlan.spring.shop.bean.ResponseFormat;
+import com.fenlan.spring.shop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    private HttpServletRequest request;
+
+    @PostMapping("/change/password")
+    public ResponseEntity<ResponseFormat> changePasswd(@RequestBody Map map) {
+        try {
+            String before = map.get("before").toString();
+            String after = map.get("after").toString();
+            userService.changePasswd(before, after);
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("change password success")
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Change failed")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<ResponseFormat> getDetailByName(@RequestParam("name") String name) {
+        try {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("query success")
+                    .path(request.getServletPath())
+                    .data(userService.findByName(name))
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Query failed")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
