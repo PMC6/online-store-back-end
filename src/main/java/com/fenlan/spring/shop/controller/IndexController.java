1da362ac8e40393a50a9a7da1cbd6b92c47094cb
fenlan <1475307818@qq.com>
2018-12-10 19:42:39 +0800

    修改权限

--- src/main/java/com/fenlan/spring/shop/controller/IndexController.java
+++ src/main/java/com/fenlan/spring/shop/controller/IndexController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -33,6 +34,8 @@ public class IndexController {
     CategoryService categoryService;
     @Autowired
     ProductService productService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("")
     public ResponseEntity<ResponseFormat> index(Authentication auth) {
@@ -162,6 +165,28 @@ public class IndexController {
         } catch (Exception e) {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
                     .error("Query error")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/change/password")
+    public ResponseEntity<ResponseFormat> changePasswd(@RequestBody Map map) {
+        try {
+            String before = map.get("before").toString();
+            String after = map.get("after").toString();
+            userService.changePasswd(before, after);
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("change password success")
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Change failed")
                     .message(e.getLocalizedMessage())
                     .path(request.getServletPath())
                     .data(null)
--- src/main/java/com/fenlan/spring/shop/controller/customer/CustomerController.java
+++ src/main/java/com/fenlan/spring/shop/controller/customer/CustomerController.java
@@ -19,28 +19,6 @@ public class CustomerController {
     @Autowired
     private HttpServletRequest request;
 
-    @PostMapping("/change/password")
-    public ResponseEntity<ResponseFormat> changePasswd(@RequestBody Map map) {
-        try {
-            String before = map.get("before").toString();
-            String after = map.get("after").toString();
-            userService.changePasswd(before, after);
-            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
-                    .error(null)
-                    .message("change password success")
-                    .path(request.getServletPath())
-                    .data(null)
-                    .build(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
-                    .error("Change failed")
-                    .message(e.getLocalizedMessage())
-                    .path(request.getServletPath())
-                    .data(null)
-                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
     @GetMapping("/detail")
     public ResponseEntity<ResponseFormat> getDetailByName(@RequestParam("name") String name) {
         try {
