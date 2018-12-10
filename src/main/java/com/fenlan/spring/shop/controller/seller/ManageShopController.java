2260a54c8e338701441912702808855df180bc1a
fanzhonghao <1412328318@qq.com>
2018-12-10 21:13:51 +0800

    update query shopinfo without seller name and add query seller info without paramter

--- src/main/java/com/fenlan/spring/shop/controller/seller/ManageShopController.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/ManageShopController.java
@@ -8,9 +8,12 @@ package com.fenlan.spring.shop.controller.seller;
 
 import com.fenlan.spring.shop.bean.ResponseFormat;
 import com.fenlan.spring.shop.service.ShopService;
+import com.fenlan.spring.shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,14 +29,17 @@ public class ManageShopController {
     ShopService shopService;
     @Autowired
     private HttpServletRequest request;
+    @Autowired
+    UserService userService;
 
     /**
      * 由卖家名得到商店信息
-     * @param sellerName
      * @return
      */
     @GetMapping("/shop/info")
-    public ResponseEntity<ResponseFormat> getShopInfo(@RequestParam("sellerName") String sellerName){
+    public ResponseEntity<ResponseFormat> getShopInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String sellerName = authentication.getName();
         try {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
                     .error(null)
@@ -42,6 +48,31 @@ public class ManageShopController {
                     .data(shopService.findByUserName(sellerName))
                     .build(), HttpStatus.OK);
         } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("find error")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 获取卖家信息
+     * @return
+     */
+    @GetMapping("/info")
+    public ResponseEntity<ResponseFormat> getSellerInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String sellerName = authentication.getName();
+        try {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("find the seller info")
+                    .path(request.getServletPath())
+                    .data(userService.findByName(sellerName))
+                    .build(), HttpStatus.OK);
+        }catch (Exception e){
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
                     .error("find error")
                     .message(e.getLocalizedMessage())
