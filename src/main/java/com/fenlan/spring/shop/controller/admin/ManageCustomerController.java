4a883dd5546fc9e1d226afa50928bd3060e91dcc
fenlan <1475307818@qq.com>
2018-12-15 15:37:20 +0800

    管理customer

---
+++ src/main/java/com/fenlan/spring/shop/controller/admin/ManageCustomerController.java
@@ -0,0 +1,61 @@
+package com.fenlan.spring.shop.controller.admin;
+
+import com.fenlan.spring.shop.bean.ResponseFormat;
+import com.fenlan.spring.shop.bean.User;
+import com.fenlan.spring.shop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/admin/customer")
+public class ManageCustomerController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/search/name")
+    public ResponseEntity<ResponseFormat> searchByName(@RequestParam("name") String name) {
+        try {
+            User customer = userService.findByNameAndRole(name, "ROLE_USER");
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("search success")
+                    .path(request.getServletPath())
+                    .data(customer)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Not found")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseFormat> deleteCustomer(@RequestParam("id") Long id) {
+        try {
+            userService.delete(id);
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("delete customer success")
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Not found")
+                    .message("delete customer failed")
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
