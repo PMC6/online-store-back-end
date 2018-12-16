8efd3c51b3ee0f4230cbd5f9f06de9979b93b66a
fenlan <1475307818@qq.com>
2018-12-16 22:21:46 +0800

    添加店铺信息修改

--- src/main/java/com/fenlan/spring/shop/controller/seller/ManageShopController.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/ManageShopController.java
@@ -8,6 +8,7 @@ package com.fenlan.spring.shop.controller.seller;
 
 import com.fenlan.spring.shop.bean.AdRequest;
 import com.fenlan.spring.shop.bean.ResponseFormat;
+import com.fenlan.spring.shop.bean.Shop;
 import com.fenlan.spring.shop.service.AdService;
 import com.fenlan.spring.shop.service.ShopService;
 import com.fenlan.spring.shop.service.UserService;
@@ -100,6 +101,25 @@ public class ManageShopController {
         } catch (Exception e) {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
                     .error("Add failed")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/shop/update")
+    public ResponseEntity<ResponseFormat> update(@RequestBody Shop shop) {
+        try {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("update success")
+                    .path(request.getServletPath())
+                    .data(shopService.update(shop))
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Update failed")
                     .message(e.getLocalizedMessage())
                     .path(request.getServletPath())
                     .data(null)
