89cc6b77f5fd953e4a1ff6551176de86eb442eb4
fenlan <1475307818@qq.com>
2018-12-16 21:11:00 +0800

    修改返回类型 Object -> ResponseFormat

--- src/main/java/com/fenlan/spring/shop/controller/customer/CartController.java
+++ src/main/java/com/fenlan/spring/shop/controller/customer/CartController.java
@@ -20,15 +20,15 @@ public class CartController {
     private HttpServletRequest request;
 
     @PostMapping("/add")
-    public ResponseEntity<Object> add(@RequestBody Map map) {
+    public ResponseEntity<ResponseFormat> add(@RequestBody Map map) {
         try {
-            Long shopid = Long.parseLong(map.get("productid").toString());
+            Long productId = Long.parseLong(map.get("id").toString());
             Integer number = Integer.parseInt(map.get("number").toString());
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
                     .error(null)
                     .message("added product in your cart")
                     .path(request.getServletPath())
-                    .data(cartService.add(shopid, number))
+                    .data(cartService.add(productId, number))
                     .build(), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
@@ -41,7 +41,7 @@ public class CartController {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Object> list() {
+    public ResponseEntity<ResponseFormat> list() {
         try {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
                     .error(null)
@@ -52,6 +52,47 @@ public class CartController {
         } catch (Exception e) {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
                     .error("Query Error")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseFormat> update(@RequestBody Map param) {
+        Long id = Long.parseLong(param.get("id").toString());
+        Integer number = Integer.parseInt(param.get("number").toString());
+        try {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("updated your cart")
+                    .path(request.getServletPath())
+                    .data(cartService.update(id, number))
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Update Error")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseFormat> delete(@RequestParam("id") Long id) {
+        try {
+            cartService.delete(id);
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("deleted one record in your cart")
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Delete Error")
                     .message(e.getLocalizedMessage())
                     .path(request.getServletPath())
                     .data(null)
