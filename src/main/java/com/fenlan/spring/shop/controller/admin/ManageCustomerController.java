260cfa231aa330a9d7008aadd6a63692381b058b
fenlan <1475307818@qq.com>
2018-12-17 16:08:59 +0800

    list users

--- src/main/java/com/fenlan/spring/shop/controller/admin/ManageCustomerController.java
+++ src/main/java/com/fenlan/spring/shop/controller/admin/ManageCustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/customer")
@@ -28,6 +29,27 @@ public class ManageCustomerController {
                     .message("search success")
                     .path(request.getServletPath())
                     .data(customer)
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("Not found")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ResponseFormat> list(@RequestParam("page") Integer page,
+                                               @RequestParam("size") Integer size) {
+        try {
+            List<User> list = userService.listCustomer(page, size);
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("list success")
+                    .path(request.getServletPath())
+                    .data(list)
                     .build(), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
