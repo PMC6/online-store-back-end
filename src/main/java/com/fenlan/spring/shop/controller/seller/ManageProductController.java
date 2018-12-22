d76485ff652777324b50f57d1e4996c90cd24bef
fanzhonghao <1412328318@qq.com>
2018-12-22 19:15:08 +0800

    add advertisement function

--- src/main/java/com/fenlan/spring/shop/controller/seller/ManageProductController.java
+++ src/main/java/com/fenlan/spring/shop/controller/seller/ManageProductController.java
@@ -360,4 +360,30 @@ public class ManageProductController {
         }
     }
 
+    /**
+     * 查看正在商场主页展示的商品
+     */
+    @GetMapping("/product/advertisement/search/applied")
+    public ResponseEntity<ResponseFormat> getByApply(@RequestParam("page") int page,
+                                                     @RequestParam("size") int size,
+                                                     @RequestParam("shopName") String shopName){
+        try {
+            Shop shop = shopService.findByName(shopName);
+            if (shop == null) throw new Exception("There doesn't has this shop");
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.OK.value())
+                    .error(null)
+                    .message("find the products")
+                    .path(request.getServletPath())
+                    .data(productService.listByApplied(page, size, shop.getId()))
+                    .build(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseFormat.Builder(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error("find failed")
+                    .message(e.getLocalizedMessage())
+                    .path(request.getServletPath())
+                    .data(null)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
